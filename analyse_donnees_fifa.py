b0d8340be93506016e2dd6e8073c5570489ca080
Exaucé MARUBA <63616448+Maruba22@users.noreply.github.com>
2022-12-10 20:04:33 +0100

    Update and rename analyse_donnees_fifa.py to analyse_donnees_titanic.py

--- analyse_donnees_fifa.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 10 19:51:05 2022
-
-@author: Exaucé Maruba
-
-J'analyse dans ce fichier Python les statistique 
-de fifa pour le Coupe du Monde
-"""
---
+++ analyse_donnees_titanic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 19:51:05 2022
+
+@author: Exaucé Maruba
+
+J'analyse dans ce fichier Python les statistique Titanic
+"""
+
+
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import matplotlib.pyplot as plt
+
+donnees = pd.read_csv('')
+print(donnees.head(6))
+
+
